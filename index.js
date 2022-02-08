1dd5e1801f7001d5924398a134e081fabc4161e7
Fadimata Tillman <fadi.tillman@gmail.com>
2022-02-08 14:37:16 -0800

    tiemer update

--- index.js
+++ index.js
@@ -7,11 +7,27 @@ class Timer {
         this.pauseButton = pauseButton;
 
         this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause)
     }
-    start(){
-        console.log('start the timer');
+    start = () => {
+        // console.log('start the timer');
+        // 1 second 
+        this.tick();
+        this.intervalId = setInterval(this.tick, 1000);
+       
+    }
+
+    pause = () => {
+        clearInterval(this.intervalId)
+    }
+
+
+    tick = () => {
+        // console.log('tick')
+        const timeLeft = parseFloat(this.timerTime.value);
     }
 }
+
    const timerInput = document.querySelector('#timerTime');
    const startButton = document.querySelector('#start');
    const pauseButton = document.querySelector('#pause');
